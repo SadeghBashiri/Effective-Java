f4c899f6cf8ac450d31641e459be820e7790a8d3
Sadegh Bashiri <Sadegh.Bashiri.dev@gmail.com>
2026-07-28 13:56:21 +0330

    feat(md): add item13

---
+++ src/main/java/org/example/item13/Address.java
@@ -0,0 +1,17 @@
+package org.example.item13;
+
+public class Address {
+    private String city;
+
+    public Address(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/org/example/item13/Main.java
@@ -0,0 +1,12 @@
+package org.example.item13;
+
+public class Main {
+    static void main() {
+        Employee e1 = new Employee("Ali", new Address("Berlin"));
+        Employee e2 = e1.clone();
+
+        e2.getAddress().setCity("Munich");
+
+        System.out.println(e1.getAddress().getCity()); // ❌ Munich
+    }
+}
